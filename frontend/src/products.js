e9c72a6c6c1b7d56763a3269e662627bb3705f02
shashank231 <65719041+shashank231@users.noreply.github.com>
2024-03-12 18:53:03 +0530

    add some data

---
+++ frontend/src/products.js
@@ -0,0 +1,16 @@
+const products = [
+    {
+        _id: '1',
+        name: 'one',
+        image: 'images/airpods.jpg',
+        description: 'desc',
+        brand: 'apple',
+        category: 'Electronics',
+        price: 100,
+        countInStock: 10,
+        rating: 5,
+        numReviews: 12,
+    }
+];
+
+export default products;
