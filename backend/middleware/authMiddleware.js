c46e62e5d9e323397935737ae53cd09c65f34eb8
shashank231 <65719041+shashank231@users.noreply.github.com>
2024-03-25 17:49:00 +0530

    authMiddleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,29 +3,31 @@ import asyncHandler from './asyncHandler.js';
 import User from '../models/userModel.js';
 
 // User must be authenticated
-const protect = asyncHandler(async (req, res, next) => {
+const protect = asyncHandler(async (req, res, next) => {    // always call next() when u r done, with what you like to do in middleware
   let token;
-
   // Read JWT from the 'jwt' cookie
-  token = req.cookies.jwt;
+  token = req.cookies.jwt;  // as in generateToken util we've set  res.cookie('jwt', token ...)
 
   if (token) {
+
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      req.user = await User.findById(decoded.userId).select('-password');
-
+      req.user = await User.findById(decoded.userId).select('-password');  // don't want password from db
       next();
     } catch (error) {
       console.error(error);
       res.status(401);
       throw new Error('Not authorized, token failed');
     }
+
   } else {
+
     res.status(401);
     throw new Error('Not authorized, no token');
+
   }
 });
+
 
 // User must be an admin
 const admin = (req, res, next) => {
@@ -36,5 +38,6 @@ const admin = (req, res, next) => {
     throw new Error('Not authorized as an admin');
   }
 };
+
 
 export { protect, admin };
